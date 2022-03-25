841613e74a463e215e3f5e99a046aec9353ee838
silviahbf <silviahfernandes@gmail.com>
2022-03-25 10:23:54 -0300

    adiciona solução do desafio02

---
+++ Desafios/Desafio02_Media2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C;
+            A = double.Parse(Console.ReadLine());
+            B = double.Parse(Console.ReadLine());
+            C = double.Parse(Console.ReadLine());
+            double Media = (A*0.2)+(B*0.3)+(C*0.5);
+            Console.WriteLine($"MEDIA = " + (Media.ToString("N1")));
+            //Console.ReadKey();
+        }
+    }
+}
+//Outra solução proposta:
+/*using System;
+
+namespace Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C;
+            A = double.Parse(Console.ReadLine());
+            if (A < 0 || A > 10)
+            {
+                Console.WriteLine("Nota inválida, digite uma nota entre 0 e 10.0");
+                A = double.Parse(Console.ReadLine());
+            }
+            B = double.Parse(Console.ReadLine());
+            if (B < 0 || B > 10)
+            {
+                Console.WriteLine("Nota inválida, digite uma nota entre 0 e 10.0");
+                B = double.Parse(Console.ReadLine());
+            }
+            C = double.Parse(Console.ReadLine());
+            if (C < 0 || C > 10)
+            {
+                Console.WriteLine("Nota inválida, digite uma nota entre 0 e 10.0");
+                C = double.Parse(Console.ReadLine());
+            }
+            double Media = (A*0.2)+(B*0.3)+(C*0.5);
+            Console.WriteLine($"MEDIA = " + (Media.ToString("N1")));
+            //Console.ReadKey();
+        }
+    }
+}*/
