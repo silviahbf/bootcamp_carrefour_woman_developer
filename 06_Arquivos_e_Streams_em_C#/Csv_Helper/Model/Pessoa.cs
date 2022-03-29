594a7a8c49621dbd733dd6c6bae051f059ea802e
Silvia Fernandes <82184943+silviahbf@users.noreply.github.com>
2022-03-29 17:20:54 -0300

    Delete 06_Arquivos_e_Streams_em_C# directory

--- 06_Arquivos_e_Streams_em_C#/Csv_Helper/Model/Pessoa.cs
+++
@@ -1,7 +0,0 @@
-public class Pessoa
-{
-    public string Nome { get; set; }
-    public string Email { get; set; }
-    public long Telefone { get; set; }
-    
-}
--- 06_Arquivos_e_Streams_em_C#/Csv_Helper/Model/Usuario.cs
+++
@@ -1,7 +0,0 @@
-public class Usuario
-{
-    public string Nome { get; set; }
-    public string Email { get; set; }
-    public long Telefone { get; set; }
-
-}
